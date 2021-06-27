f2933c6125c5ba4289da5d93063e17ab334b4e7d
minibox24 <minibox724@gmail.com>
2021-06-27 18:19:40 +0900

    :hammer: jishaku 추가

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 from discord.ext import commands
 
 
-bot = commands.Bot(command_prefix='!')
-bot.load_extension('poll')
+bot = commands.Bot(command_prefix="!")
+bot.load_extension("poll")
+bot.load_extension("jishaku")
+
 
 @bot.event
 async def on_ready():
-    print('Ready')
+    print("Ready")
+
 
 bot.run("NTEyOTA2OTg4NDUyMTg0MDY0.W-5_2w.vnuBi9eYSOgIlSBH6OfIPoR4OWo")
