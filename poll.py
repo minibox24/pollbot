c872167c0650a58d89f83028aaafb540dfea2526
minibox24 <minibox724@gmail.com>
2021-06-27 21:09:59 +0900

    :sparkles: 투표 시스템

--- poll.py
+++ poll.py
@@ -1,12 +1,14 @@
 import discord
 from discord.ext import commands
 from discord.http import Route
-from utils import dump_data, make_buttons
+from utils import dump_data, make_buttons, parse_msg, parse_data
+from itertools import chain
 
 
 class Poll(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.state = bot._connection
 
     @commands.command("ping")
     async def ping(self, ctx):
@@ -26,14 +28,82 @@ class Poll(commands.Cog):
             color=0x58D68D,
         )
 
-        route = Route(
-            "POST", "/channels/{channel_id}/messages", channel_id=ctx.channel.id
-        )
         await self.bot.http.request(
-            route,
+            Route("POST", "/channels/{channel_id}/messages", channel_id=ctx.channel.id),
             json={
                 "embed": embed.to_dict(),
-                "components": make_buttons(elements, dump_data([[] * len(elements)])),
+                "components": make_buttons(
+                    elements, dump_data([[] for _ in range(len(elements))])
+                ),
+            },
+        )
+
+    @commands.Cog.listener()
+    async def on_socket_response(self, msg):
+        if msg["t"] != "INTERACTION_CREATE":
+            return
+
+        (
+            message,
+            user,
+            custom_id,
+            components,
+            interaction_id,
+            interaction_token,
+        ) = parse_msg(msg["d"], self.state)
+
+        data = parse_data(components)
+
+        if not data:
+            return
+
+        choose = list(filter(lambda x: x["id"] == custom_id, components))[0]
+
+        if user.id in data[choose["index"]]:
+            content = "투표를 취소했습니다!"
+            data[choose["index"]].remove(user.id)
+        elif user.id in list(chain.from_iterable(data)):
+            index = list(filter(lambda x: user.id in x[1], enumerate(data)))[0][0]
+            data[index].remove(user.id)
+            data[choose["index"]].append(user.id)
+            content = f"{components[index]['label']}에서 {choose['label']}로 투표했습니다!"
+        else:
+            content = f"{choose['label']}에 투표했습니다!"
+            data[choose["index"]].append(user.id)
+
+        embed = message.embeds[0]
+        embed.description = "\n".join(
+            map(lambda x: f"`{x['label']}` : {len(data[x['index']])}표", components)
+        )
+
+        await self.bot.http.request(
+            Route(
+                "PATCH",
+                "/channels/{channel_id}/messages/{message_id}",
+                channel_id=message.channel[0].id,
+                message_id=message.id,
+            ),
+            json={
+                "embed": embed.to_dict(),
+                "components": make_buttons(
+                    list(map(lambda x: x["label"], components)), dump_data(data)
+                ),
+            },
+        )
+
+        await self.bot.http.request(
+            Route(
+                "POST",
+                "/interactions/{id}/{token}/callback",
+                id=interaction_id,
+                token=interaction_token,
+            ),
+            json={
+                "type": 4,
+                "data": {
+                    "content": content,
+                    "flags": 64,
+                },
             },
         )
 
