f5ba6fb153f769409fc8f6af5052923d3f14b3c0
minibox24 <minibox724@gmail.com>
2021-06-27 21:34:26 +0900

    :sparkles: Sqlite ORM 추가

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from tortoise import Tortoise
 
 
 bot = commands.Bot(command_prefix="!")
@@ -8,6 +9,8 @@ bot.load_extension("jishaku")
 
 @bot.event
 async def on_ready():
+    await Tortoise.init(db_url="sqlite://db.sqlite3", modules={"models": ["model"]})
+    await Tortoise.generate_schemas()
     print("Ready")
 
 
